eaf14a4df5f6aac9409a211812f6db2deb83256f
lnugraha <leo.nugraha@outlook.com>
2021-03-04 16:40:39 +0800

    Initial Commit

---
+++ swiftification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftification
+//
+//  Created by Leo Nugraha on 2021/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
